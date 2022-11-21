69d4096a4926665e907b375437beccf8c9e71ff4
linshujie <13437850023@163.com>
2022-11-21 09:09:14 +0800

    新增一些算法实现

--- datastructure/src/main/java/com/shujie/datastructure/queue/ArrayQueueDemo.java
+++ datastructure/src/main/java/com/shujie/datastructure/queue/ArrayQueueDemo.java
@@ -1,5 +1,7 @@
 package com.shujie.datastructure.queue;
 
+import java.util.Scanner;
+
 /**
  * @date: 2022/11/20
  * @author: linshujie
@@ -7,6 +9,46 @@ package com.shujie.datastructure.queue;
 public class ArrayQueueDemo {
 
 
+    public static void main(String[] args) {
+        ArrayQueue arrayQueue = new ArrayQueue(3);
+        char key = ' ';
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while (loop){
+            System.out.println("s(show):显示队列");
+            System.out.println("e(exit):退出程序");
+            System.out.println("a(add):添加数据到队列");
+            System.out.println("p(pop):从队列取出数据");
+            System.out.println("h(head):查看队列头的数据");
+            key = scanner.next().charAt(0);
+            switch (key){
+                case 's':
+                    arrayQueue.show();
+                    break;
+                case 'a':
+                    System.out.println("输出一个数");
+                    int value = scanner.nextInt();
+                    arrayQueue.add(value);
+                    break;
+                case 'p':
+                    int res = arrayQueue.pop();
+                    System.out.printf("取出的队列头数据是%d\n",res);
+                    break;
+                case 'h':
+                    int res2 = arrayQueue.peek();
+                    System.out.printf("队列头的数据是%d\n",res2);
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        System.out.println("exit");
+    }
 }
 
 class ArrayQueue {
