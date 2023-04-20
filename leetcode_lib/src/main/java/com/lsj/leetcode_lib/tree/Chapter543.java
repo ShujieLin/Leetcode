cd77afce3d07104967881ab85ec4e2ff31f68760
linshujie <13437850023@163.com>
2023-04-20 09:22:39 +0800

    二叉树

--- leetcode_lib/src/main/java/com/lsj/leetcode_lib/tree/Chapter543.java
+++ leetcode_lib/src/main/java/com/lsj/leetcode_lib/tree/Chapter543.java
@@ -1,40 +1,41 @@
-//package com.lsj.leetcode_lib.tree;
-//
-///**
-// * @author linshujie
-// */
-//public class Chapter543 {
-//    public static void main(String[] args) {
-////        TreeNode node = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
-//        TreeNode node = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
-//
-//        int diameter = new Solution().diameterOfBinaryTree(node);
-//        System.out.println("diameter = " + diameter);
-//
-//    }
-//
-//    static class Solution {
-//
-//        public int diameterOfBinaryTree(TreeNode root) {
-//            int leftDepth = traverse(root.left);
-//            int rightDepth = traverse(root.right);
-//            System.out.println("rightDepth = " + rightDepth);
-//            System.out.println("leftDepth = " + leftDepth);
-//            return Math.max(leftDepth, rightDepth);
-//        }
-//
-//        private int traverse(TreeNode root) {
-//            if (root == null) return 0;
-//            int leftDepth = maxDepth(root.left);
-//            int rightDepth = maxDepth(root.right);
-//            return Math.max(leftCount, rightCount);
-//        }
-//
-//        private int maxDepth(TreeNode treeNode) {
-//            if (treeNode == null) return 0;
-//            int left = maxDepth(treeNode.left);
-//            int right = maxDepth(treeNode.right);
-//            return Math.max(left,right);
-//        }
-//    }
-//}
+package com.lsj.leetcode_lib.tree;
+
+/**
+ * @author linshujie
+ */
+public class Chapter543 {
+    public static void main(String[] args) {
+//        TreeNode node = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        TreeNode node = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+
+        /*int diameter = new Solution().diameterOfBinaryTree(node);
+        System.out.println("diameter = " + diameter);*/
+
+        new Solution().maxDepth(node);
+    }
+
+    static class Solution {
+        int result = 0;
+        /**
+         * 所谓二叉树的直径，就是左右子树的最大深度之和，那么直接的想法是对每个节点计算左右子树的最大高度，得出每个节点的直径，从而得出最大的那个直径。
+         * @param root
+         * @return
+         */
+        public int diameterOfBinaryTree(TreeNode root) {
+
+            return 0;
+        }
+
+        int maxDepth(TreeNode node){
+            if (node == null) return 0;
+            int leftDepth = maxDepth(node.left);
+            int rightDepth = maxDepth(node.right);
+            System.out.println("rightDepth = " + rightDepth);
+            System.out.println("leftDepth = " + leftDepth);
+
+            return 1 + Math.max(leftDepth , rightDepth);
+
+        }
+
+    }
+}
