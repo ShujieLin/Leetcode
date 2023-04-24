41609c95f6de23011529803f589ead0298e8cdf8
linshujie <13437850023@163.com>
2023-04-24 18:53:14 +0800

    同步

--- leetcode_lib/src/main/java/com/lsj/leetcode_lib/tree/Chapter114.java
+++ leetcode_lib/src/main/java/com/lsj/leetcode_lib/tree/Chapter114.java
@@ -8,12 +8,34 @@ public class Chapter114 {
 
     }
 
+    static class Solution {
+        public void flatten(TreeNode root) {
+            if (root == null) return;
+
+            flatten(root.left);
+            flatten(root.right);
+            //后序遍历:
+            //把左子树放到该节点的右边，让后把左子树置空
+            TreeNode leftNode = root.left;
+            TreeNode rightNode = root.right;
+            root.left = null;
+            root.right = leftNode;
+
+            //建立指针，找到右子树的底部
+            TreeNode p = root;
+            while (p.right !=null){
+                p = p.right;
+            }
+            p.right = rightNode;
+        }
+    }
+
     /**
      * 但是注意 flatten 函数的签名，返回类型为 void，也就是说题目希望我们在原地把二叉树拉平成链表。
      *
      * 这样一来，没办法通过简单的二叉树遍历来解决这道题了。
      */
-    static class Solution {
+    static class Solution2 {
         TreeNode dummy = new TreeNode(-1);
         TreeNode p = dummy;//用于辅助制作链表的指针
 
---
+++ leetcode_lib/src/main/java/com/lsj/leetcode_lib/tree/Chapter654.java
@@ -0,0 +1,40 @@
+package com.lsj.leetcode_lib.tree;
+
+/**
+ * @author linshujie
+ */
+public class Chapter654 {
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{3,2,1,6,0,5};
+        new Solution().constructMaximumBinaryTree(nums);
+
+    }
+
+    static class Solution {
+        public TreeNode constructMaximumBinaryTree(int[] nums) {
+            return buildNode(nums, 0, nums.length - 1);
+        }
+
+        private TreeNode buildNode(int[] nums, int lowIndex, int highIndex) {
+            if (lowIndex > highIndex) return null;
+
+            //找到数组的最大值
+            int value = Integer.MIN_VALUE;
+            int index = 0;
+            for (int i = lowIndex; i < highIndex; i++) {
+                if (value < nums[i]){
+                    value = nums[i];
+                    index = i;
+                }
+            }
+
+            //新建结点
+            TreeNode root = new TreeNode(value);
+            //连接左右结点
+            root.left = buildNode(nums,lowIndex,index - 1);
+            root.right = buildNode(nums,index + 1,highIndex);
+            return root;
+        }
+    }
+}
