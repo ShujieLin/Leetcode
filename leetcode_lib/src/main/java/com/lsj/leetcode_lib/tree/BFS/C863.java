8cb6d1edc9e9b2660bc88d846c4ff246c79918d8
linshujie <13437850023@163.com>
2023-05-24 11:27:00 +0800

    同步

--- leetcode_lib/src/main/java/com/lsj/leetcode_lib/tree/BFS/C863.java
+++ leetcode_lib/src/main/java/com/lsj/leetcode_lib/tree/BFS/C863.java
@@ -2,11 +2,15 @@ package com.lsj.leetcode_lib.tree.BFS;
 
 import com.lsj.leetcode_lib.tree.TreeNode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 
 /**
  * @author linshujie
@@ -14,12 +18,74 @@ import java.util.Queue;
 public class C863 {
 
     public static void main(String[] args) {
-
+        TreeNode node = new TreeNode(3,
+                new TreeNode(5,
+                        new TreeNode(6), 
+                        new TreeNode(2,
+                                new TreeNode(7), new TreeNode(4))),
+                new TreeNode(1, 
+                        new TreeNode(0), new TreeNode(8)));
+        List<Integer> list = new Solution2().distanceK(node, node.left, 2);
+        System.out.println("Arrays.toString(list.toArray()) = " + Arrays.toString(list.toArray()));
     }
 
-    class Solution2 {
+    static class Solution2 {
+
+        private Map<Integer, TreeNode> parents;
+
         public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-            return null;
+
+            return bfs(root, target, k);
+        }
+
+        private List<Integer> bfs(TreeNode root, TreeNode target, int k) {
+            List<Integer> res = new ArrayList<>();
+            //转换为图,树里面没有重复value的节点,可以把value作为key
+            parents = new HashMap<>();
+            traverse(root, null);
+
+            Queue<TreeNode> q = new LinkedList<>();
+            Set<TreeNode> visited = new HashSet<>();
+            int distance = 0;
+            q.offer(target);
+            visited.add(target);
+
+            while (!q.isEmpty()) {
+                int size = q.size();
+
+                for (int i = 0; i < size; i++) {
+                    TreeNode cur = q.poll();
+                    if (distance == k) {
+                        res.add(cur.val);
+                        continue;
+                    }
+
+                    if (cur.left != null && !visited.contains(cur.left)) {
+                        q.offer(cur.left);
+                        visited.add(cur.left);
+                    }
+                    if (cur.right != null && !visited.contains(cur.right)) {
+                        q.offer(cur.right);
+                        visited.add(cur.right);
+                    }
+                    //向上，左，右三个方向奔跑
+                    TreeNode curParent = parents.get(cur.val);
+                    if (curParent != null && !visited.contains(curParent)) {
+                        q.offer(curParent);
+                        visited.add(curParent);
+                    }
+                }
+                distance += 1;
+            }
+
+            return res;
+        }
+
+        private void traverse(TreeNode root, TreeNode parent) {
+            if (root == null) return;
+            parents.put(root.val, parent);
+            traverse(root.left, root);
+            traverse(root.right, root);
         }
     }
 
