060652a6569b41cb7b15089767a316e52e721d3d
linshujie <13437850023@163.com>
2023-07-20 08:55:04 +0800

    更新

--- leetcode_lib/src/main/java/com/lsj/leetcode_lib/jianzhi/JZ014.java
+++ leetcode_lib/src/main/java/com/lsj/leetcode_lib/jianzhi/JZ014.java
@@ -1,5 +1,8 @@
 package com.lsj.leetcode_lib.jianzhi;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author linshujie
  */
@@ -15,20 +18,38 @@ public class JZ014 {
         public boolean checkInclusion(String s1, String s2) {
             int left = 0, right = 0;
 
-            String sub ;
+            Map<Character, Integer> window = new HashMap<>();
+            Map<Character, Integer> need = new HashMap<>();
+            for (char c :
+                    s1.toCharArray()) {
+                need.put(c, need.getOrDefault(c, 0));
+            }
             boolean res = false;
+            int valid = 0;
             while (right < s2.length()) {
-                right ++;
-                sub = s2.substring(left, right);
-                System.out.println("sub = " + sub);
-
-                while (!sub.contains(s1) && left < right){
-                    left ++;
-                    sub = s2.substring(left,right);
+                char c1 = s2.charAt(right);
+                right++;
+                if (need.containsKey(c1)) {
+                    window.put(c1, window.getOrDefault(c1, 0) + 1);
+                    if (need.get(c1).equals(window.get(c1))) valid++;
                 }
 
-                if (sub.equals(s1)) res = true;
+                while (right - left > s1.length()) {
+                    System.out.println("valid = " + valid);
+                    char c2 = s2.charAt(left);
+                    left++;
+                    if (need.containsKey(c2)) {
+                        window.put(c2, window.getOrDefault(c2, 0) - 1);
+                        if (need.get(c2).equals(window.get(c2))) valid--;
+                    }
+                }
+
+
+                System.out.println("s2 = " + s2.substring(left,right));
+
+
             }
+
             return res;
         }
     }
