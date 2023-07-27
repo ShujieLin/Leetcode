ae5f0a83cb56ccec1930de39ee8fb9c9c336e533
linshujie <13437850023@163.com>
2023-07-27 09:01:28 +0800

    更新

---
+++ leetcode_lib/src/main/java/com/lsj/leetcode_lib/jianzhi/JZ090.java
@@ -0,0 +1,46 @@
+package com.lsj.leetcode_lib.jianzhi;
+
+/**
+ * @author: linshujie
+ */
+public class JZ090 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,3,2};
+        int[] nums2 = new int[]{1,2,3,1};
+        int rob = new Solution().rob(nums2);
+        System.out.println("rob = " + rob);
+    }
+
+    static class Solution {
+        public int rob(int[] nums) {
+
+            if (nums.length == 0) return 0;
+            //动态规划
+
+            //dp 第一个偷
+            int[] dp = new int[nums.length];
+            dp[0] = nums[0];
+            dp[1] = Math.max(nums[0], nums[1]);
+            //转移方程 最后一个不能偷
+            for (int i = 2; i < nums.length - 1; i++) {
+                //dp[i]代表可以偷盗的最大金额
+                //偷：dp[i] = dp[i - 2] + nums[i]
+                //不偷：dp[i] = dp[i-1]
+                //每次更新最大值
+                //Math.max{dp[i - 2] + num[i] , dp[i - 1]}
+                dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+            }
+
+            //第一个不偷
+            int[] dp2 = new int[nums.length];
+            dp2[0] = 0;
+            dp2[1] = nums[1];
+            for (int i = 2; i < nums.length; i++) {
+                dp2[i] = Math.max(dp2[i - 2] + nums[i], dp2[i - 1]);
+            }
+
+
+            return Math.max(dp2[nums.length - 1], dp[nums.length - 1]);
+        }
+    }
+}
