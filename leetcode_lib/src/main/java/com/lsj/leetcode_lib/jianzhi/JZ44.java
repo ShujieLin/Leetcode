ea72332a740632c5070dadba444760e8229569f3
linshujie <13437850023@163.com>
2023-08-13 08:58:49 +0800

    更新

---
+++ leetcode_lib/src/main/java/com/lsj/leetcode_lib/jianzhi/JZ44.java
@@ -0,0 +1,62 @@
+package com.lsj.leetcode_lib.jianzhi;
+
+import com.lsj.leetcode_lib.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author: linshujie
+ */
+public class JZ44 {
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1,
+                new TreeNode(3,
+                        new TreeNode(5),
+                        new TreeNode(3)),
+                new TreeNode(2,
+                        null,
+                        new TreeNode(9)));
+        List<Integer> list = new Solution().largestValues(null);
+        System.out.println("Arrays.toString(list) = " + Arrays.toString(list.toArray()));
+    }
+
+    static class Solution {
+
+        private List<Integer> res;
+
+        public List<Integer> largestValues(TreeNode root) {
+            res = new ArrayList<>();
+            bfs(root);
+            return res;
+        }
+
+        private void bfs(TreeNode root) {
+            if (root == null) return;
+
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+
+            while (!queue.isEmpty()) {
+                int max = Integer.MIN_VALUE;
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode cur = queue.poll();
+                    max = Math.max(max, cur.val);
+
+                    if (cur.left != null) {
+                        queue.offer(cur.left);
+                    }
+                    if (cur.right != null) {
+                        queue.offer(cur.right);
+                    }
+                }
+                res.add(max);
+            }
+        }
+
+    }
+}
